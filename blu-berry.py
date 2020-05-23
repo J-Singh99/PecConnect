55d00a006c4027616c04b4968f09f964f454c6cb
Jaspreet Singh <jaspreet099@gmail.com>
2020-05-23 14:46:51 +0530

    with templates

--- blu-berry.py
+++ blu-berry.py
@@ -2,10 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-app.route('/')
+@app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home Page</h1>"
+    return ""
 
 
 @app.route('/about')
